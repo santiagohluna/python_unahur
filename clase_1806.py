138b319680a51e3cf0a836a43ff09340b2cc9911
Santiago Luna <santiagohluna@gmail.com>
2024-06-18 19:36:04 -0300

    Clase del 18 del 6

---
+++ clase_1806.py
@@ -0,0 +1,30 @@
+# Clase del martes 18 de junio de 2024
+
+## Números enteros = int o long
+
+## long 5465465465465132165L
+
+## Float = números con decimales
+
+## Complex 33.45j
+
+#Procedencia de operaciones
+# 1) Términos entre paréntesis
+# 2) Potencia y radicación
+# 3) Multiplicación y división
+# 4) Suma y resta
+
+# Cadenas de caracteres (Strings = str)
+
+a = '¡Hola Mundo!'
+
+print(a+'\n\t'+a)
+
+# \n = salto de línea
+# \t = tabulación
+
+# Variables
+## En Python las variables se interpretan como etiquetas que permiten hacer referencia a los datos
+## Por cada dato que aparece en un programa, Pyhton crea un objeto que lo contiene y cada objeto tiene:
+# (1) un identificador único (id), 
+# (2) un tipo de dato,
